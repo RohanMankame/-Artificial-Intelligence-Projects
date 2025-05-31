fbf5479a46a4f6434e4dc1ff8c9512b86029407d
Rohan Mankame <rohanmankame@outlook.com>
2025-05-31 08:16:50 +0530

    Update and rename hw2_utils.py to Utils.py

--- Project 2/Utils.py
+++ Project 2/Utils.py
@@ -1,13 +1,4 @@
-########################################################
-#
-# CMPSC 441: Homework 2
-#
-########################################################
 
-
-#
-# DO NOT CHANGE ANYTHING IN THIS FILE.
-# - Your submission will be tested using this file
 #   in its original content.
 #
 
